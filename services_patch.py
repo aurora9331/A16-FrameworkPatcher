3cd4dd47a3c00f6f08e504d68ed38126a053f69e
aurora9331 <sevincemre993@gmail.com>
2025-08-09 13:24:17 +0300

    Update services_patch.py

--- services_patch.py
+++ services_patch.py
@@ -5,6 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 def patch(filepath):
     with open(filepath, 'r') as file:
         lines = file.readlines()
@@ -29,7 +30,7 @@ def patch(filepath):
 
             if line.strip() == '.end method':
                 if method_type in method_patterns:
-                    logging.info(f"{method_type} metodu gövdesi temizleniyor")
+                    logging.info(f"Clearing method body for {method_type}")
                     modified_lines.append(registers_line)
                     if method_type == "hashCode":
                         modified_lines.append("    const/4 v0, 0x0\n")
@@ -38,7 +39,7 @@ def patch(filepath):
                         modified_lines.append("    const/4 v0, 0x0\n")
                         modified_lines.append("    return v0\n")
                     elif method_type == "toString":
-                        modified_lines.append("    const/4 v0, 0x0\n")
+                        modified_lines.append("     const/4 v0, 0x0\n")
                         modified_lines.append("    return-object v0\n")
                 in_method = False
                 method_type = None
@@ -48,10 +49,10 @@ def patch(filepath):
 
         for key, pattern in method_patterns.items():
             if pattern.search(line):
-                logging.info(f"{key} metodu bulundu. Metot içeriği temizleniyor.")
+                logging.info(f"Found method {key}. Clearing method content.")
                 in_method = True
                 method_type = key
-                modified_lines.append(line)  # Metot deklarasyonunu ekle
+                modified_lines.append(line)  # Add method declaration to output
                 break
 
         if not in_method:
@@ -59,10 +60,11 @@ def patch(filepath):
 
     with open(filepath, 'w') as file:
         file.writelines(modified_lines)
-    logging.info(f"Dosya için düzenleme tamamlandı: {filepath}")
+    logging.info(f"Completed modification for file: {filepath}")
     
+
 def modify_file(file_path):
-    logging.info(f"Dosya düzenleniyor: {file_path}")
+    logging.info(f"Modifying file: {file_path}")
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
@@ -73,15 +75,13 @@ def modify_file(file_path):
 
     method_patterns = {
         "checkDowngrade": re.compile(
-            r'\.method\s+public\s+static\s+checkDowngrade\(Lcom/android/server/pm/pkg/AndroidPackage;Landroid/content/pm/PackageInfoLite;\)V'),
-        "shouldCheckUpgradeKeySetLocked": re.compile(
-            r'\.method\s+public\s+shouldCheckUpgradeKeySetLocked\(Lcom/android/server/pm/pkg/PackageStateInternal;Lcom/android/server/pm/pkg/SharedUserApi;I\)Z'),
+            r'\.method public static checkDowngrade\(Lcom/android/server/pm/pkg/AndroidPackage;Landroid/content/pm/PackageInfoLite;\)V'),
+        "shouldCheckUpgradeKeySetLocked": re.compile(r'\.method public shouldCheckUpgradeKeySetLocked\(Lcom/android/server/pm/pkg/PackageStateInternal;Lcom/android/server/pm/pkg/SharedUserApi;I\)Z'),
         "verifySignatures": re.compile(
-            r'\.method\s+public\s+static\s+verifySignatures\(Lcom/android/server/pm/PackageSetting;Lcom/android/server/pm/SharedUserSetting;Lcom/android/server/pm/PackageSetting;Landroid/content/pm/SigningDetails;Landroid/content/pm/SigningDetails;\)I'),
+            r'\.method public static verifySignatures\(Lcom/android/server/pm/PackageSetting;Lcom/android/server/pm/SharedUserSetting;Lcom/android/server/pm/PackageSetting;Landroid/content/pm/SigningDetails;ZZZ\)Z'),
         "compareSignatures": re.compile(
-            r'\.method\s+public\s+static\s+compareSignatures\(Landroid/content/pm/SigningDetails;Landroid/content/pm/SigningDetails;\)I'),
-        "matchSignaturesCompat": re.compile(
-            r'\.method.*matchSignaturesCompat\(.*\)Z')
+            r'\.method public static compareSignatures\(Landroid/content/pm/SigningDetails;Landroid/content/pm/SigningDetails;\)I'),
+        "matchSignaturesCompat": re.compile(r'\.method.*matchSignaturesCompat\(.*\)Z')
     }
 
     for line in lines:
@@ -89,7 +89,7 @@ def modify_file(file_path):
             if line.strip() == '.end method':
                 modified_lines.append(method_start_line)
                 if method_type == "checkDowngrade":
-                    logging.info(f"{method_type} metodu gövdesi düzenleniyor")
+                    logging.info(f"Modifying method body for {method_type}")
                     modified_lines.append("    .registers 2\n")
                     modified_lines.append("    .annotation system Ldalvik/annotation/Throws;\n")
                     modified_lines.append("        value = {\n")
@@ -98,13 +98,13 @@ def modify_file(file_path):
                     modified_lines.append("    .end annotation\n")
                     modified_lines.append("    return-void\n")
                 elif method_type == "shouldCheckUpgradeKeySetLocked":
-                    logging.info(f"{method_type} metodu gövdesi düzenleniyor")
-                    original_registers_line = "    .registers 10\n"
+                    logging.info(f"Modifying method body for {method_type}")
+                    original_registers_line = "    .registers 10\n"  # Define the line here
                     modified_lines.append(original_registers_line)
                     modified_lines.append("    const/4 v0, 0x0\n")
                     modified_lines.append("    return v0\n")
                 elif method_type == "verifySignatures":
-                    logging.info(f"{method_type} metodu gövdesi düzenleniyor")
+                    logging.info(f"Modifying method body for {method_type}")
                     modified_lines.append("    .registers 21\n")
                     modified_lines.append("    .annotation system Ldalvik/annotation/Throws;\n")
                     modified_lines.append("        value = {\n")
@@ -114,12 +114,12 @@ def modify_file(file_path):
                     modified_lines.append("    const/4 v1, 0x0\n")
                     modified_lines.append("    return v1\n")
                 elif method_type == "compareSignatures":
-                    logging.info(f"{method_type} metodu gövdesi düzenleniyor")
+                    logging.info(f"Modifying method body for {method_type}")
                     modified_lines.append("    .registers 3\n")
                     modified_lines.append("    const/4 v0, 0x0\n")
                     modified_lines.append("    return v0\n")
                 elif method_type == "matchSignaturesCompat":
-                    logging.info(f"{method_type} metodu gövdesi düzenleniyor")
+                    logging.info(f"Modifying method body for {method_type}")
                     modified_lines.append("    .registers 5\n")
                     modified_lines.append("    const/4 v0, 0x1\n")
                     modified_lines.append("    return v0\n")
@@ -140,12 +140,13 @@ def modify_file(file_path):
 
     with open(file_path, 'w') as file:
         file.writelines(modified_lines)
-    logging.info(f"Dosya düzenlemesi tamamlandı: {file_path}")
+    logging.info(f"Completed modification for file: {file_path}")
     
+
 def modify_reconcile_package_utils(file_path):
     """
-    Smali dosyasını düzenleyerek, belirtilen invoke-static satırından sonra ilk `const/4 v0, 0x0` satırını
-    `const/4 v0, 0x1` olarak değiştir.
+    Modify the smali file to change the first occurrence of `const/4 v0, 0x0` 
+    to `const/4 v0, 0x1` after the specified invoke-static line.
     """
     target_line = "invoke-static {}, Lcom/android/internal/hidden_from_bootclasspath/android/content/pm/Flags;->restrictNonpreloadsSystemShareduids()Z"
     found_target = False
@@ -158,30 +159,30 @@ def modify_reconcile_package_utils(file_path):
     for line in lines:
         modified_lines.append(line)
         if target_line in line:
-            logging.info(f"Hedef satır bulundu: {line.strip()}")
+            logging.info(f"Found target line: {line.strip()}")
             found_target = True
             continue
         
         if found_target and "const/4 v0, 0x0" in line and not modified:
-            # İlk `const/4 v0, 0x0` satırını `const/4 v0, 0x1` olarak değiştir
-            logging.info(f"Satır düzenleniyor: {line.strip()}")
+            # Replace the first occurrence of `const/4 v0, 0x0` with `const/4 v0, 0x1`
+            logging.info(f"Modifying line: {line.strip()}")
             modified_lines[-1] = "    const/4 v0, 0x1\n"
             modified = True
 
     if not found_target:
-        logging.warning(f"Hedef satır dosyada bulunamadı: {file_path}")
+        logging.warning(f"Target line not found in file: {file_path}")
     elif not modified:
-        logging.warning(f"Hedef satırdan sonra `const/4 v0, 0x0` bulunamadı: {file_path}")
+        logging.warning(f"No `const/4 v0, 0x0` found after the target line in file: {file_path}")
 
     with open(file_path, 'w') as file:
         file.writelines(modified_lines)
 
     if modified:
-        logging.info(f"Dosya başarıyla düzenlendi: {file_path}")
+        logging.info(f"File modified successfully: {file_path}")
 
 
 def modify_install_package_helper(file_path):
-    logging.info(f"{file_path} içinde preparePackageLI düzenleniyor")
+    logging.info(f"Modifying preparePackageLI in {file_path}")
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
@@ -193,12 +194,12 @@ def modify_install_package_helper(file_path):
 
     for i, line in enumerate(lines):
         if re.match(r'\.method.*preparePackageLI\(.*\)', line) and "private" in line:
-            logging.info("Metot bulundu: preparePackageLI.")
+            logging.info("Found the method: preparePackageLI.")
             in_method = True
 
         if in_method:
-            if re.search(r'invoke-interface \{p5\}, Lcom/android/server/pm/pkg/AndroidPackage;->isLeavingSharedUser\(\)Z', line):
-                logging.info(f"invoke-interface satırı bulundu satır {i + 1}: {line.strip()}")
+            if re.search(r'invoke-interface \{v7\}, Lcom/android/server/pm/pkg/AndroidPackage;->isLeavingSharedUser\(\)Z', line):
+                logging.info(f"Found invoke-interface at line {i + 1}: {line.strip()}")
                 const_string_index = i
                 break
 
@@ -209,23 +210,24 @@ def modify_install_package_helper(file_path):
                     target_register = match.group(1)
 
     if last_if_eqz_index is not None and const_string_index is not None and last_if_eqz_index < const_string_index:
-        logging.info(f"'if-eqz' satırı düzenleniyor satır {last_if_eqz_index + 1}: {lines[last_if_eqz_index].strip()}")
+        logging.info(f"Modifying 'if-eqz' at line {last_if_eqz_index + 1}: {lines[last_if_eqz_index].strip()}")
         modified_lines = (
             lines[:last_if_eqz_index]
             + [f"    const/4 {target_register}, 0x1\n"]
             + lines[last_if_eqz_index:]
         )
     else:
-        logging.warning("const-string öncesinde geçerli bir 'if-eqz' bulunamadı.")
+        logging.warning("Failed to find a valid 'if-eqz' before the const-string.")
         modified_lines = lines
 
     with open(file_path, 'w') as file:
         file.writelines(modified_lines)
-    logging.info(f"{file_path} içinde preparePackageLI düzenlemesi tamamlandı")
+    logging.info(f"Completed modification for preparePackageLI in {file_path}")
+
 
 def modify_smali_files(directories):
     for directory in directories:
-        logging.info(f"Dizin taranıyor: {directory}")
+        logging.info(f"Scanning directory: {directory}")
         for root, _, files in os.walk(directory):
             for file in files:
                 if file.endswith(".smali"):
@@ -239,34 +241,34 @@ def modify_smali_files(directories):
         verify_Signatures = os.path.join(directory, 'com/android/server/pm/PackageManagerServiceUtils.smali')
 
         if os.path.exists(package_manager_service_utils):
-            logging.info(f"Dosya bulundu: {package_manager_service_utils}")
+            logging.info(f"Found file: {package_manager_service_utils}")
             modify_file(package_manager_service_utils)
         else:
-            logging.warning(f"Dosya bulunamadı: {package_manager_service_utils}")
+            logging.warning(f"File not found: {package_manager_service_utils}")
 
         if os.path.exists(Key_Set_Manager_Service):
-            logging.info(f"Dosya bulundu: {Key_Set_Manager_Service}")
+            logging.info(f"Found file: {Key_Set_Manager_Service}")
             modify_file(Key_Set_Manager_Service)
         else:
-            logging.warning(f"Dosya bulunamadı: {Key_Set_Manager_Service}")
+            logging.warning(f"File not found: {Key_Set_Manager_Service}")
 
         if os.path.exists(verify_Signatures):
-            logging.info(f"Dosya bulundu: {verify_Signatures}")
+            logging.info(f"Found file: {verify_Signatures}")
             modify_file(verify_Signatures)
         else:
-            logging.warning(f"Dosya bulunamadı: {verify_Signatures}")
+            logging.warning(f"File not found: {verify_Signatures}")
 
         if os.path.exists(install_package_helper):
-            logging.info(f"Dosya bulundu: {install_package_helper}")
+            logging.info(f"Found file: {install_package_helper}")
             modify_install_package_helper(install_package_helper)
         else:
-            logging.warning(f"Dosya bulunamadı: {install_package_helper}")
+            logging.warning(f"File not found: {install_package_helper}")
 
         if os.path.exists(reconcile_package_utils):
-            logging.info(f"Dosya bulundu: {reconcile_package_utils}")
+            logging.info(f"Found file: {reconcile_package_utils}")
             modify_reconcile_package_utils(reconcile_package_utils)
         else:
-            logging.warning(f"Dosya bulunamadı: {reconcile_package_utils}")
+            logging.warning(f"File not found: {reconcile_package_utils}")
 
 
 if __name__ == "__main__":
